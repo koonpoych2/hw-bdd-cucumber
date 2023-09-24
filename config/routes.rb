8548bd22276c65a20b70c0277bee117f9f3e70c9
Anecha Khumjun <68006281+koonpoych2@users.noreply.github.com>
2023-09-24 17:01:08 +0700

    sso

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rottenpotatoes::Application.routes.draw do
   post '/movies/search_tmdb' => 'movies#search_tmdb', :as => 'search_tmdb'
   resources :movies
   # map '/' to be a redirect to '/movies'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
   root :to => redirect('/movies')
 end
